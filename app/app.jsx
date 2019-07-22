6b2d838685f19cd2fa08006b931fa0c2c5121add
julia-jel <44364383+julia-jel@users.noreply.github.com>
2019-07-22 16:56:30 +0300

    Create app.jsx

---
+++ app/app.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+/* Import Components */
+import Presentational from './components/Presentational';
+
+ReactDOM.render(<Presentational />, document.getElementById("root"));
